648899a5333c91523493c1da2a8fe4de8486b62d
MirzaArshad2023 <mirza.arshad@institutedata.com>
2025-10-16 22:40:24 +1300

    Code updated with more exercises

---
+++ IOD/Module1/ClassExercises/sample.js
@@ -0,0 +1,25 @@
+function PrintLinesToConsole() {
+  console.log("Hello world");
+  console.log("This is javascript");
+  console.log("I'm learning");
+  console.log("Basics of javascript");
+  console.log("Hello world");
+  console.log("Hello world");
+  console.log("Hello world");
+  console.log("Hello world");
+}
+function Sum(a, b) {
+  return a + b;
+}
+
+function Mulitply(a, b) {
+  return a * b;
+}
+function Subtract(a, b) {
+  return a - b;
+}
+function Divide(a, b) {
+  return a / b;
+}
+
+console.log(Mulitply(Sum(2, 3), Sum(11, 5)));
