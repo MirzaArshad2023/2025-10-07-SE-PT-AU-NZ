12d8d835753a1c6f4254c7815702fddef15c4a95
MirzaArshad2023 <mirza.arshad@institutedata.com>
2025-10-18 11:07:03 +1300

    Backend code
    Backend code

--- IOD/Module1/ClassExercises/Backend/index.js
+++ IOD/Module1/ClassExercises/Backend/index.js
@@ -1,1 +1,4 @@
-console.log("Hello Node JS");
+const Sentiment = require("sentiment");
+const sentiment = new Sentiment();
+const result = sentiment.analyze("Economy is disaster.");
+console.log(result); // Score: -2, Comparative: -0.666
